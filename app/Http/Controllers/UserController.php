e4d5a97ac68e93351998da7350c96c8dbcfad308
Sherifdeen <jimohsherifdeen6@gmail.com>
2021-01-23 07:05:09 -0800

    initial commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+
+        return  User::orderby('id', 'DESC')->get();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+       
+       $checfIfExist = User::where('email', $request->input('email'))->first();
+
+       if ($checfIfExist ) {
+         return 'user already exist';
+       }else{
+        $create =  User::create([
+                        'name' => $request->input('name'),
+                        'email' =>  $request->input('email'),
+                        'password' => Hash::make($request->input('password'))]);
+        return 'user created successfull';
+       }
+       
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+               $findUser = User::find($id);
+
+               if ($findUser) {
+                return $findUser;
+               }
+        return array('status' => 404, 'message' => 'no user found');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+      $delete =   User::find($id)->delete();
+
+
+      if ($delete) {
+         return 'user deleted successfully'
+      }
+    }
+}
